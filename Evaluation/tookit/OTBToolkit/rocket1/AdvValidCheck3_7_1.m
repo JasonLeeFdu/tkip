f78069b2b2aa1f7d1dce59920a55b2468bf8ffe6
JasonLeeFdu <jasonleefdu@vip.sina.com>
2019-01-11 22:49:35 +0800

    T

--- Evaluation/tookit/OTBToolkit/rocket1/AdvValidCheck3_7_1.m
+++ Evaluation/tookit/OTBToolkit/rocket1/AdvValidCheck3_7_1.m
@@ -93,7 +93,7 @@ videosList = videosList(3:end);
 
 
 
-for idxVideo=3:3:length(videosList) %% Here to do the paralell things
+for idxVideo=2:3:length(videosList) %% Here to do the paralell things
     %% get the imgSet
     videoClip = fullfile(datasetBase,videosList(idxVideo).name,'img') ;   
     imgSet = {};
