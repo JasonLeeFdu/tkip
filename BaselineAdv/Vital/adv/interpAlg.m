997e653bfa6984e74ddd5a6239789214f39b040b
JasonLeeFdu <jasonleefdu@vip.sina.com>
2019-01-24 11:11:43 +0800

    Y201901241100

--- BaselineAdv/Vital/adv/interpAlg.m
+++ BaselineAdv/Vital/adv/interpAlg.m
@@ -5,30 +5,66 @@ function [Res] = interpAlg(imgSet,To)
 % This simulate version is just the utilize the change of the exact file
 % name
 
+
+if To < 0
+    %resolve To
+    p1 = strfind(path,'/');
+    p2 = strfind(path,'.');
+    To = str2double(path(p1(end)+1:p2(end)-1));
+    token = imgSet;
+else
+    token = imgSet{To};
+end
+
+
+
+
 dsRate = 2;
 resIdx = To -1;
 if dsRate == 2
    resIdx = 2*To - 2;
-   
 end
 
-Res = imgSet(To);
-token = imgSet{1};
-tail = strfind(token,'/');
-tail = tail(end);
-path = token(1:tail-1);
-file = token(tail+1:end);
-dot  = strfind(file,'.');
-fn   = file(1:dot-1);
+
+
+
 ext  = file(dot+1:end);
-n    = length(fn);
-format = ['%0' num2str(n) 'd'  ];
-targetFile = sprintf([format '.' ext],resIdx);
-targetPath  = [path(1:32) 'OriginalInterp2' path(41:end)];
 
-Res = imread(fullfile(targetPath,targetFile));
+OTB100_FRM_OPT_PATH = '/home/winston/workSpace/PycharmProjects/tracking/TrackingGuidedInterpolation/Datasets/OriginalInterp2/OTB100/';
+TempleColor128_FRM_OPT_PATH = '/home/winston/workSpace/PycharmProjects/tracking/TrackingGuidedInterpolation/Datasets/OriginalInterp2/TempleColor128/';
+VOT2016_FRM_OPT_PATH = '/home/winston/workSpace/PycharmProjects/tracking/TrackingGuidedInterpolation/Datasets/OriginalInterp2/VOT2016';
 
 
+
+if length(strfind(token,'OTB')) > 0 %%%
+    n = 4;
+    format = ['%0' num2str(n) 'd'  ];
+    targetFile = sprintf([format '.' ext],resIdx);
+    targetPath  = OTB100_FRM_OPT_PATH;
+    tmp = token;
+    poses = strfind(tmp,'/');
+    videoName = tmp(poses(end-2)+1:poses(end-1)-1);
+    videoName = fullfile(videoName,'img');
+elseif length(strfind(token,'olor128')) > 0 %%%
+    n = 4;
+    format = ['%0' num2str(n) 'd'  ];
+    targetFile = sprintf([format '.' ext],resIdx);
+    targetPath  = TempleColor128_FRM_OPT_PATH;
+    tmp = token;
+    poses = strfind(tmp,'/');
+    videoName = tmp(poses(end-2)+1:poses(end-1)-1);
+elseif length(strfind(lower(token),'vot')) > 0 %%%
+    n = 8;
+    format = ['%0' num2str(n) 'd'  ];
+    targetFile = sprintf([format '.' ext],resIdx);
+    targetPath  = VOT2016_FRM_OPT_PATH;
+    tmp = token;
+    poses = strfind(tmp,'/');
+    videoName = tmp(poses(end-1)+1:poses(end)-1);
+
+end
+
+Res = imread(fullfile(targetPath,videoName,targetFile));
 
 end
 
--- BaselineAdv/Vital/adv/optF.m
+++ BaselineAdv/Vital/adv/optF.m
@@ -5,11 +5,23 @@ function res = optF(imgSet,To,type)
 % This simulate version is just the utilize the change of the exact file
 % name
 %% load, instead of calculation
-dsRate = 2;
-resIdx = To -1;
 
 % test:
 %% OTB100-frm
+
+%% ONLY for vot converters
+if To < 0
+    %resolve To
+    p1 = strfind(path,'/');
+    p2 = strfind(path,'.');
+    To = str2double(path(p1(end)+1:p2(end)-1));
+    token = imgSet;
+else
+    token = imgSet{To};
+end
+
+
+resIdx = To -1;
 
 
 
@@ -18,77 +30,69 @@ resIdx = To -1;
 OTB100_FRM_OPT_PATH = '/home/winston/workSpace/PycharmProjects/tracking/TrackingGuidedInterpolation/Datasets/Original/OTB100_optFlow/';
 OTB100_ITP_OPT_PATH = '/home/winston/workSpace/PycharmProjects/tracking/TrackingGuidedInterpolation/Datasets/OriginalInterp2/OTB100_optflow_itpNori/';
 VOT2016_FRM_OPT_PATH = '/home/winston/workSpace/PycharmProjects/tracking/TrackingGuidedInterpolation/Datasets/Original/vot2016_optFlow/';
-VOT2016_ITP_OPT_PATH = '';
+VOT2016_ITP_OPT_PATH = '/home/winston/Datasets/Tracking/OriginalInterp2/VOT2016_optflow_itpNori/';
 TempleColor128_FRM_OPT_PATH = '/home/winston/workSpace/PycharmProjects/tracking/TrackingGuidedInterpolation/Datasets/Original/TempleColor128_optFlow/';
-TempleColor128_ITP_OPT_PATH = '';
+TempleColor128_ITP_OPT_PATH = '/home/winston/Datasets/Tracking/OriginalInterp2/TempleColor128_optflow_itpNori/';
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-
-Res = imgSet(To);
-token = imgSet{1};
-tail = strfind(token, '/');
-tail = tail(end);
-path = token(1:tail-1);
-file = token(tail+1:end);
-dot  = strfind(file,'.');
-fn   = file(1:dot-1);
 ext  = 'mat';
 
+
 if strcmp(type,'frm')
-    if length(strfind(imgSet{To},'OTB')) > 0
+    if length(strfind(token,'OTB')) > 0 %%%
         n = 4;
         format = ['%0' num2str(n) 'd'  ];
         targetFile = sprintf([format '_5.' ext],resIdx);
         targetPath  = OTB100_FRM_OPT_PATH;
-        tmp = imgSet{To};
+        tmp = token;
         poses = strfind(tmp,'/');
         videoName = tmp(poses(end-2)+1:poses(end-1)-1);
-    elseif length(strfind(imgSet{To},'olor128')) > 0
+    elseif length(strfind(token,'olor128')) > 0 %%%
         n = 4;
         format = ['%0' num2str(n) 'd'  ];
         targetFile = sprintf([format '_5.' ext],resIdx);
         targetPath  = TempleColor128_FRM_OPT_PATH;
-        tmp = imgSet{To};
+        tmp = token;
         poses = strfind(tmp,'/');
         videoName = tmp(poses(end-2)+1:poses(end-1)-1);
-    elseif length(strfind(lower(imgSet{To}),'vot')) > 0
+    elseif length(strfind(lower(token),'vot')) > 0 %%%
         n = 8;
         format = ['%0' num2str(n) 'd'  ];
         targetFile = sprintf([format '_5.' ext],resIdx);
         targetPath  = VOT2016_FRM_OPT_PATH;
-        tmp = imgSet{To};
+        tmp = token;
         poses = strfind(tmp,'/');
-        videoName = tmp(poses(end-2)+1:poses(end-1)-1);
+        videoName = tmp(poses(end-1)+1:poses(end)-1);
         
     end
 elseif strcmp(type,'itp') 
     resIdx = To;
-    if length(strfind(imgSet{To},'OTB')) > 0
+    if length(strfind(token,'OTB')) > 0 %%%
         n = 4;
         format = ['%0' num2str(n) 'd'  ];
         targetFile = sprintf([format '__5.' ext],resIdx);
         targetPath  = OTB100_ITP_OPT_PATH;
-        tmp = imgSet{To};
+        tmp = token;
         poses = strfind(tmp,'/');
         videoName = tmp(poses(end-2)+1:poses(end-1)-1);
-    elseif length(strfind(imgSet{To},'olor128')) > 0
+    elseif length(strfind(token,'olor128')) > 0
         n = 4;
         format = ['%0' num2str(n) 'd'  ];
         targetFile = sprintf([format '__5.' ext],resIdx);
         targetPath  = TempleColor128_ITP_OPT_PATH;
-        tmp = imgSet{To};
+        tmp = token;
         poses = strfind(tmp,'/');
         videoName = tmp(poses(end-2)+1:poses(end-1)-1);
-    elseif length(strfind(lower(imgSet{To}),'vot')) > 0
+    elseif length(strfind(lower(token),'vot')) > 0
         n = 8;
         format = ['%0' num2str(n) 'd'  ];
         targetFile = sprintf([format '__5.' ext],resIdx);
         targetPath  = VOT2016_ITP_OPT_PATH;
-        tmp = imgSet{To};
+        tmp = token;
         poses = strfind(tmp,'/');
-        videoName = tmp(poses(end-2)+1:poses(end-1)-1);
+        videoName = tmp(poses(end-1)+1:poses(end)-1);
     end
 end
 
