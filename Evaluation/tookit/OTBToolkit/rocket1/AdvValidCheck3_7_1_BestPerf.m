d50186bf914aa2142a922e2761c84257363e3ed8
jasonlee <jasonleefdu@vip.sina.com>
2019-04-10 07:52:43 +0800

    T

--- Evaluation/tookit/OTBToolkit/rocket1/AdvValidCheck3_7_1_BestPerf.m
+++ Evaluation/tookit/OTBToolkit/rocket1/AdvValidCheck3_7_1_BestPerf.m
@@ -100,12 +100,7 @@ videosList = videosList(3:end);
 doneFlagVid = false;
 
 
-<<<<<<< HEAD
-for idxVideo= 3:3:length(videosList)% 对于每一个视频(此处可以使用多进程)  
-=======
 for idxVideo= 1:length(videosList)% 对于每一个视频(此处可以使用多进程)  
-
->>>>>>> 5d22ce5385c95407214416db62e4b1f856f539f9
      disp([ '================== AdvBaseline Validation check fixed version1: ADV' ' --- ' ', ' num2str(idxVideo) '_' videosList(idxVideo).name '================== '])       
      completeFileName = sprintf('%s_%s_Adv.mat',videosList(idxVideo).name,trackers{1}.name);
      if exist(fullfile(resPathBase,completeFileName),'file')  && (~overWrite)
